6fc3655cd4567d15a553137524400a07640e8dcb
anasbenmguirida <anasbenmki@gmail.com>
2024-08-26 13:03:31 +0100

    resolving a cross origins blocked error

--- src/main/java/com/example/demo/products/ProductController.java
+++ src/main/java/com/example/demo/products/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:4200")
 
 public class ProductController {
+
     private final ProductService ps;
 
     @Autowired
